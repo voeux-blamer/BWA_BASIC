c16a84bf22615eac312cfd5276f387309e63174f
voeux-blamer <voeuxrochefort@gmail.com>
2023-09-04 09:07:11 +0700

    done flutter basic

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
